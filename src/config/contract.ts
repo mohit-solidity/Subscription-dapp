0b9a233b41182756dead555a601c98399937a428
Mohit <mohit96046@gmail.com>
2026-02-06 16:50:12 +0530

    update contract address

--- src/config/contract.ts
+++ src/config/contract.ts
@@ -1,4 +1,4 @@
-export const CONTRACT_ADDRESS = "0xf04b989FfcAa939e9867E7280afa6FEE90770Ae2" as const;
+export const CONTRACT_ADDRESS = "0xA211e393a24018b052EEE7C58D6479598a07ea7F" as const;
 export const OWNER_ADDRESS = "0xb4df6ac663383fb70bf1171d10f458c41933f85b" as const;
 
 // Assuming Sepolia for now, but this should be matched with the chain ID the contract is deployed on.
